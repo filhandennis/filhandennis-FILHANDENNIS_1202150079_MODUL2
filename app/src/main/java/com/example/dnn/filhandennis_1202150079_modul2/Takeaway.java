1675b526c298a47d75a8477f794f8f234ffa6409
Xandor <filhandennis@gmail.com>
2018-02-18 23:36:03 +0700

    Add Fragment

--- app/src/main/java/com/example/dnn/filhandennis_1202150079_modul2/Takeaway.java
+++ app/src/main/java/com/example/dnn/filhandennis_1202150079_modul2/Takeaway.java
@@ -1,9 +1,11 @@
 package com.example.dnn.filhandennis_1202150079_modul2;
 
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class Takeaway extends AppCompatActivity {
 
@@ -18,5 +20,35 @@ public class Takeaway extends AppCompatActivity {
     public void selectMenu2(View view){
         //Pindah Activity
         startActivity(new Intent(this, DaftarMenu.class));
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), getString(R.string.date_picker));
+    }
+
+    public void showTimePickerDialog(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), getString(R.string.time_picker));
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        // The month integer returned by the date picker starts counting at 0
+        // for January, so you need to add 1 to show months starting at 1.
+        String month_string = Integer.toString(month + 1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        // Assign the concatenated strings to dateMessage.
+        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+        Toast.makeText(this, ""+ dateMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    public void processTimePickerResult(int hourOfDay, int minute) {
+        // Convert time elements into strings.
+        String hour_string = Integer.toString(hourOfDay);
+        String minute_string = Integer.toString(minute);
+        // Assign the concatenated strings to timeMessage.
+        String timeMessage = (hour_string + ":" + minute_string);
+        Toast.makeText(this, ""+ timeMessage, Toast.LENGTH_SHORT).show();
     }
 }
